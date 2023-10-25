52e709ae241957526bf3113533db4f91ae032948
Trịnh Khánh <realkripper@gmail.com>
2023-10-25 08:37:20 +0700

    add catchAsync

--- src/route/todo.router.js
+++ src/route/todo.router.js
@@ -1,10 +1,10 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const todoController = require('../controller/todo.controller');
-
+const { catchAsync } = require('../util/catchAsync')
 router
-    .get('/', todoController.getTodos)
-    .post('/', todoController.createTodo)
-    .put('/:todoID', todoController.updateTodo)
-    .delete('/:todoID', todoController.deleteTodo);
+    .get('/', catchAsync(todoController.getTodos))
+    .post('/', catchAsync(todoController.createTodo))
+    .put('/:todoID', catchAsync(todoController.updateTodo))
+    .delete('/:todoID', catchAsync(todoController.deleteTodo));
 
 module.exports = router;
